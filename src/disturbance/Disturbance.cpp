c2c5d9c078d3ba27ad176cc0b64469e2f45b1354
Osman <osmanozevin@gmail.com>
2021-05-19 13:43:46 +0100

    switch to python

--- src/disturbance/Disturbance.cpp
+++
@@ -1,6 +0,0 @@
-
-#include "Disturbance.hpp"
-
-void disturbanceHandler::parseCoAPMessage () {;}; // parses coap message
-void disturbanceHandler::parseMQTTMessage () {;}; // parses mqtt message
-void disturbanceHandler::handleDisturbance () {;}; // send message to machinery to prepare for incoming disturbance
--- src/mqtt/MQTT.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "MQTT.hpp"
-
-void MQTTReceiver::connectToBroker () {;}; // connects to MQTT broker
-void MQTTReceiver::subscribeToTopic () {;}; // subscribe to MQTT topics
-void MQTTReceiver::receiveMessage () {;}; // handle incoming messages
-
-void MQTTSender::publishToTopic () {;}; // publish msg to given topic
