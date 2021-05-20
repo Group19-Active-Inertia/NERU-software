66691c911bc01fb63abc6394868079e81b4cf87a
Osman <osmanozevin@gmail.com>
2021-05-20 13:11:29 +0100

    implemented coap server and requests

--- classes/coap.py
+++ classes/coap.py
@@ -1,29 +1,60 @@
-
+import aiocoap.resource as resource
+from aiocoap import *
+import asyncio
+from multiprocessing import Process
 
 class CoAP:
     
     def __init__ (self, radius:int):
         self.nearbyNERURadius = radius
         self.nearbyNERUs = []
+        
+        self.protocol = Context.create_client_context()
+        
+        # Need to use multiprocessing because it makes the server non-blocking
+        self.server = Process(target=self.startServer).start()
     
-    def startServer (self):
-        pass
+    def startServer(self):
+        server = resource.Site()
+        server.add_resource(["d"], Disturbance())
+        
+        context = Context.create_server_context(server)
+        asyncio.Task(context)
+        
+        asyncio.get_event_loop().run_forever()
     
-    def handleIncomingMessage (self):
-        pass
-    
-    def dispatchDisturbanceMessage (self):
-        pass
+    def dispatchDisturbanceMessages(self, disturbance):
+        
+        async def dispatchMessage(ip,port):
+            msg = Message(code=POST,
+                          mtype=NON,
+                          payload=disturbance,
+                          uri=f'coap://{ip}:{port}/d')
+            self.protocol.request(msg)
+            
+        for (ip,port) in self.nearbyNERUs:
+            dispatchMessage(ip,port)
+            
     
     # calculate nearby nerus from list of nerus
-    def setNearbyNERUs (self, nerus:dict):
+    def setNearbyNERUs(self, nerus:dict):
         pass
     
     # calculate if neru is nearby enough and add to list
-    def addNearbyNERU (self, neru:dict):
+    def addNearbyNERU(self, neru:dict):
         pass
     
     # remove nearby NERU from list
-    def removeNearbyNERU (self, neru:str):
+    def removeNearbyNERU(self, neru:str):
         pass
     
+    
+# handles incoming disturbances from CoAP
+class Disturbance (resource.Resource):
+    async def render_get(self, request):
+        # handle incoming disturbance
+        # e.g.
+        #
+        # if not_prepared_for_disturbance:
+        #    prepare_for_disturbance()
+        return
